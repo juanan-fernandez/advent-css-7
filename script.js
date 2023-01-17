314fe2a93c04ceae49aa3206944bc4837bc9cedf
juanan fernandez <juanan.fernandez@gmail.com>
2023-01-17 21:01:47 +0100

    css final

---
+++ script.js
@@ -0,0 +1,8 @@
+const amount = document.getElementById('amount');
+const people = document.getElementById('people');
+const tipAmount = document.querySelectorAll('.amount')[0];
+const tipPerPerson = document.querySelectorAll('.amount')[1];
+
+function clickValButton() {
+	console.log(this);
+}
