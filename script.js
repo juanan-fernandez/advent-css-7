12b7c5ffb6182859e4102baad856a5263a658b26
juanan fernandez <juanan.fernandez@gmail.com>
2023-01-18 14:05:13 +0100

    css final

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const amount = document.getElementById('amount');
+const amount = document.getElementById('amountBill');
 const people = document.getElementById('people');
 const tipAmount = document.querySelectorAll('.amount')[0];
 const tipPerPerson = document.querySelectorAll('.amount')[1];
 
-function clickValButton() {
-	console.log('any');
+handleAmountBillChange = ev => {
+	console.log(ev.target);
+};
+
+amount.addEventListener('onchange', handleAmountBillChange);
+
+function calcTip(amount, persons, percent) {
+	const tip = (amount * percent) / 100;
+	const amountPerPerson = tip / persons;
+	tipAmount.innerHTML = `<sup>$</sup>${formatNumbers(tip)}`;
+	tipPerPerson.innerHTML = `<sup>$</sup>${formatNumbers(amountPerPerson)}`;
+	return;
 }
+
+function formatNumbers(myNumber) {
+	const f = new Intl.NumberFormat('en-us', {
+		currency: 'USD',
+		maximumFractionDigits: 2,
+		minimumFractionDigits: 2,
+	});
+	return f.format(myNumber);
+}
+
+function clickValButton(obj) {
+	console.log(obj);
+	console.log(amount);
+	console.log(tipAmount.innerHTML);
+	console.log(tipPerPerson);
+}
+
+calcTip(1000, 3, 5);
