bb7994624f42687575eaa961be98a34928cd8946
juanan fernandez <juanan.fernandez@gmail.com>
2023-01-18 15:36:05 +0100

    final version

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const amount = document.getElementById('amountBill');
+const amountBill = document.getElementById('amountBill');
 const people = document.getElementById('people');
+let percentVal = 0;
 const tipAmount = document.querySelectorAll('.amount')[0];
 const tipPerPerson = document.querySelectorAll('.amount')[1];
 
-handleAmountBillChange = ev => {
-	console.log(ev.target);
+const handleAmountBillChange = ev => {
+	ev.target.value = formatInputNumbers(Number(ev.target.value));
 };
 
-amount.addEventListener('onchange', handleAmountBillChange);
+amountBill.addEventListener('blur', handleAmountBillChange);
 
 function calcTip(amount, persons, percent) {
 	const tip = (amount * percent) / 100;
-	const amountPerPerson = tip / persons;
+	let amountPerPerson = 0;
+	if (tip > 0 && persons > 0) amountPerPerson = tip / persons;
 	tipAmount.innerHTML = `<sup>$</sup>${formatNumbers(tip)}`;
 	tipPerPerson.innerHTML = `<sup>$</sup>${formatNumbers(amountPerPerson)}`;
 	return;
 }
 
+function formatInputNumbers(inputNumber) {
+	return (Math.round(inputNumber * 100) / 100).toFixed(2);
+}
+
 function formatNumbers(myNumber) {
-	const f = new Intl.NumberFormat('en-us', {
+	const f = new Intl.NumberFormat('sp-es', {
 		currency: 'USD',
 		maximumFractionDigits: 2,
 		minimumFractionDigits: 2,
@@ -26,11 +32,20 @@ function formatNumbers(myNumber) {
 	return f.format(myNumber);
 }
 
-function clickValButton(obj) {
-	console.log(obj);
-	console.log(amount);
-	console.log(tipAmount.innerHTML);
-	console.log(tipPerPerson);
+function clickValButton(tipPercentValue) {
+	console.log(tipPercentValue);
+	const percentButtons = document.querySelectorAll('.buttons__button');
+
+	percentButtons.forEach(btn => {
+		btn.classList.remove('buttons__button--selected');
+		if (btn.value === tipPercentValue) {
+			console.log(btn.value);
+			btn.classList.add('buttons__button--selected');
+		}
+	});
 }
 
-calcTip(1000, 3, 5);
+function handleClickCalculate() {
+	const percentVal = document.querySelector('.buttons__button--selected').value;
+	calcTip(amountBill.value, people.value, percentVal);
+}
